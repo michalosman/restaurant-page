8e413d9d0adef1de8a37f2cd0d3812116787c18d
michalosman <michal.osman.997@gmail.com>
2021-07-28 16:56:19 +0200

    Fix contact page

--- src/contact.js
+++ src/contact.js
@@ -1,28 +1,28 @@
 function createContact() {
-  const contact = document.createElement("div");
-  contact.classList.add("contact");
+  const contact = document.createElement('div')
+  contact.classList.add('contact')
 
-  const phoneNumber = document.createElement("p");
-  phoneNumber.textContent = "Phone: 123 456 789";
+  const phoneNumber = document.createElement('p')
+  phoneNumber.textContent = '📞 123 456 789'
 
-  const address = document.createElement("p");
-  address.textContent = "Addres: Hollywood Boulevard 42, Los Angeles, USA";
+  const address = document.createElement('p')
+  address.textContent = '🏠 Hollywood Boulevard 42, Los Angeles, USA'
 
-  const restaurantLocation = document.createElement("img");
-  restaurantLocation.src = "images/restaurant-location.png";
-  restaurantLocation.alt = "Mozzafiato restaurant location";
+  const restaurantLocation = document.createElement('img')
+  restaurantLocation.src = 'images/restaurant-location.png'
+  restaurantLocation.alt = 'Mozzafiato restaurant location'
 
-  contact.appendChild(phoneNumber);
-  contact.appendChild(address);
-  contact.appendChild(restaurantLocation);
+  contact.appendChild(phoneNumber)
+  contact.appendChild(address)
+  contact.appendChild(restaurantLocation)
 
-  return contact;
+  return contact
 }
 
 function loadContact() {
-  const main = document.getElementById("main");
-  main.textContent = "";
-  main.appendChild(createContact());
+  const main = document.getElementById('main')
+  main.textContent = ''
+  main.appendChild(createContact())
 }
 
-export default loadContact;
+export default loadContact
